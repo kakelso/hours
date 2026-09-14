5a303042a3e0e7fc284e0a62e0d0d09c2967051f
kakelso <kakelso@gmail.com>
2026-09-14 19:44:07 -0400

    Add files via upload

---
+++ prayers.js
@@ -0,0 +1,35 @@
+// prayers.js
+// Notice we use backticks ( ` ) so we can write on multiple lines!
+const prayerLibrary = {
+    0: [ 
+        `O Lord, our heavenly Father, almighty and everlasting God, you have brought us safely to the beginning of this day: Defend us by your mighty power, that we may not fall into sin nor run into any danger; but that all our doings may be ordered by your governance, to do always what is righteous in your sight; through Jesus Christ our Lord. Amen.`,
+        
+        `O Lord, our heavenly Father, almighty and everlasting God, you have brought us safely to the beginning of this day: Defend us by your mighty power, that we may not fall into sin nor run into any danger; but that all our doings may be ordered by your governance, to do always what is righteous in your sight; through Jesus Christ our Lord. Amen.`
+    ],
+    3: [ 
+        `O God, who sent your Holy Spirit upon the apostles in the third hour with fiery tongues: grant us, we pray, by the same Spirit to have a right judgment in all things evermore, and to rejoice in his holy comfort; through the merits of Christ Jesus our Savior, who lives and reigns with you, in the unity of the same Spirit, one God, world without end. Amen.`,
+		
+		`Lord Jesus Christ, you said to your apostles, "Peace I give to you; my own peace I leave with you:" Regard not our sins, but the faith of your Church, and give to us the peace and unity of that heavenly City, where with the Father and the Holy Spirit you live and reign, now and for ever. Amen.`
+    ],
+    6: [ 
+        `Blessed Savior, at this hour you hung upon the Cross, stretching out your loving arms: Grant that all the peoples of the earth may look to you and be saved; for your tender mercies’ sake. Amen.`,
+		
+		`Almighty Savior, who at mid-day called your servant Saint Paul to be an apostle to the Gentiles: We pray you to illumine the world with the radiance of your glory, that all nations may come and worship you; for you live and reign with the Father and the Holy Spirit, one God, for ever and ever. Amen.`
+    ],
+    9: [ 
+        `O God, who sustain the world with your power and guide it with your love: Grant that as the day declines, your light may not fail us, but that by your grace we may finish our daily tasks and be brought safely to the evening; through Christ our Lord. Amen.`,
+		
+		`Pour your grace into our hearts, O Lord, that we who have known the incarnation of your Son Jesus Christ, announced by an angel to the Virgin Mary, may by his Cross and passion be brought to the glory of his resurrection; who lives and reigns with you, in the unity of the Holy Spirit, one God, now and for ever. Amen.`
+    ],
+    12: [ 
+        `O gladsome light,
+pure brightness of the everliving Father in heaven,
+	O Jesus Christ, holy and blessed!
+Now as we come to the setting of the sun,
+and our eyes behold the vesper light,
+	we sing your praises, O God: Father, Son, and Holy Spirit.
+You are worthy at all times to be praised by happy voices,
+	O Son of God, O Giver of Life,
+	and to be glorified through all the worlds.`
+    ]
+};
