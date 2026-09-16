a79e88b96a92198152f0304a1899f636d80bba17
kakelso <kakelso@gmail.com>
2026-09-16 09:52:28 -0400

    Update prayers.js
    Added The Lord's Prayer to all Sext prayers.

--- prayers.js
+++ prayers.js
@@ -32,19 +32,124 @@ const prayerLibrary = {
 		`Teach me to seek you, and as I seek you, show yourself to me; for I cannot seek you unless you show me how, and I will never find you unless you show yourself to me. Let me seek you by desiring you, and desire you by seeking you; let me find you by loving you, and love you in finding you. Amen.` // Saturday - Anselm of Canterbury
     ],
     6: [ 
-        `Almighty Savior, who at mid-day called your servant Saint Paul to be an apostle to the Gentiles: We pray you to illumine the world with the radiance of your glory, that all nations may come and worship you; for you live and reign with the Father and the Holy Spirit, one God, for ever and ever. Amen.`, // Sunday
+        `Almighty Savior, who at mid-day called your servant Saint Paul to be an apostle to the Gentiles: We pray you to illumine the world with the radiance of your glory, that all nations may come and worship you; for you live and reign with the Father and the Holy Spirit, one God, for ever and ever. Amen.
 		
-		`Father of all mercies, you revealed your boundless compassion to your apostle Saint Peter in a three-fold vision: Forgive our unbelief, we pray, and so strengthen our hearts and enkindle our zeal, that we may fervently desire the salvation of all people, and diligently labor in the extension of your kingdom; through him who gave himself for the life of the world, your Son our Savior Jesus Christ. Amen.`, // Monday
+Our Father, who art in heaven,
+	Hallowed be thy Name,
+	thy kingdom come,
+	thy will be done, 
+	on earth as it is in heaven.
+Give us this day our daily bread.
+And forgive us our trespasses,
+	as we forgive those
+	who trespass against us.
+And lead us not into temptation,
+	but deliver us from evil.
+For thine is the kingdom, 
+	and the power, and the glory,
+	for ever and ever. Amen.`, // Sunday
 		
-		`O God, the light of the minds that know you, the life of the souls that love you, and the strength of the wills that serve you: Help us so to know you that we may truly love you, and so to love you that we may fully serve you, whom to serve is perfect freedom; through Jesus Christ our Lord. Amen.`, // Tuesday
+		`Father of all mercies, you revealed your boundless compassion to your apostle Saint Peter in a three-fold vision: Forgive our unbelief, we pray, and so strengthen our hearts and enkindle our zeal, that we may fervently desire the salvation of all people, and diligently labor in the extension of your kingdom; through him who gave himself for the life of the world, your Son our Savior Jesus Christ. Amen.
 		
-		`Almighty God, you sent your Son Jesus Christ to reconcile the world to yourself: We praise and bless you for those whom you have sent in the power of the Spirit to preach the Gospel to all nations. We thank you that in all parts of the earth a community of love has been gathered together by their prayers and labors, and that in every place your servants call upon your Name; for the kingdom and the power and the glory are yours, for ever and ever. Amen.`, // Wednesday
+Our Father, who art in heaven,
+	Hallowed be thy Name,
+	thy kingdom come,
+	thy will be done, 
+	on earth as it is in heaven.
+Give us this day our daily bread.
+And forgive us our trespasses,
+	as we forgive those
+	who trespass against us.
+And lead us not into temptation,
+	but deliver us from evil.
+For thine is the kingdom, 
+	and the power, and the glory,
+	for ever and ever. Amen.`, // Monday
 		
-		`We give you thanks, most gracious God, for the beauty of earth and sky and sea; for the richness of mountains, plains, and rivers; for the wonder of your creatures, large and small; and for all the loveliness that surrounds us. We praise you for these good gifts, and pray that we may safeguard them for our posterity. Grant that we may continue to grow in our grateful enjoyment of your abundant creation, to the honor and glory of your Name, now and for ever. Amen.`, // Thursday
+		`O God, the light of the minds that know you, the life of the souls that love you, and the strength of the wills that serve you: Help us so to know you that we may truly love you, and so to love you that we may fully serve you, whom to serve is perfect freedom; through Jesus Christ our Lord. Amen.
 		
-		`Blessed Savior, at this hour you hung upon the Cross, stretching out your loving arms: Grant that all the peoples of the earth may look to you and be saved; for your tender mercies’ sake. Amen.`, // Friday
+Our Father, who art in heaven,
+	Hallowed be thy Name,
+	thy kingdom come,
+	thy will be done, 
+	on earth as it is in heaven.
+Give us this day our daily bread.
+And forgive us our trespasses,
+	as we forgive those
+	who trespass against us.
+And lead us not into temptation,
+	but deliver us from evil.
+For thine is the kingdom, 
+	and the power, and the glory,
+	for ever and ever. Amen.`, // Tuesday
 		
-		`O God, of your goodness, give me yourself, for you are enough for me. I can ask for nothing less that is completely to your honor, and if I do ask anything less, I shall always be in want. Only in you I have all. Amen.` // Saturday - Julian of Norwich
+		`Almighty God, you sent your Son Jesus Christ to reconcile the world to yourself: We praise and bless you for those whom you have sent in the power of the Spirit to preach the Gospel to all nations. We thank you that in all parts of the earth a community of love has been gathered together by their prayers and labors, and that in every place your servants call upon your Name; for the kingdom and the power and the glory are yours, for ever and ever. Amen.
+		
+Our Father, who art in heaven,
+	Hallowed be thy Name,
+	thy kingdom come,
+	thy will be done, 
+	on earth as it is in heaven.
+Give us this day our daily bread.
+And forgive us our trespasses,
+	as we forgive those
+	who trespass against us.
+And lead us not into temptation,
+	but deliver us from evil.
+For thine is the kingdom, 
+	and the power, and the glory,
+	for ever and ever. Amen.`, // Wednesday
+		
+		`We give you thanks, most gracious God, for the beauty of earth and sky and sea; for the richness of mountains, plains, and rivers; for the wonder of your creatures, large and small; and for all the loveliness that surrounds us. We praise you for these good gifts, and pray that we may safeguard them for our posterity. Grant that we may continue to grow in our grateful enjoyment of your abundant creation, to the honor and glory of your Name, now and for ever. Amen.
+		
+Our Father, who art in heaven,
+	Hallowed be thy Name,
+	thy kingdom come,
+	thy will be done, 
+	on earth as it is in heaven.
+Give us this day our daily bread.
+And forgive us our trespasses,
+	as we forgive those
+	who trespass against us.
+And lead us not into temptation,
+	but deliver us from evil.
+For thine is the kingdom, 
+	and the power, and the glory,
+	for ever and ever. Amen.`, // Thursday
+		
+		`Blessed Savior, at this hour you hung upon the Cross, stretching out your loving arms: Grant that all the peoples of the earth may look to you and be saved; for your tender mercies’ sake. Amen.
+		
+Our Father, who art in heaven,
+	Hallowed be thy Name,
+	thy kingdom come,
+	thy will be done, 
+	on earth as it is in heaven.
+Give us this day our daily bread.
+And forgive us our trespasses,
+	as we forgive those
+	who trespass against us.
+And lead us not into temptation,
+	but deliver us from evil.
+For thine is the kingdom, 
+	and the power, and the glory,
+	for ever and ever. Amen.`, // Friday
+		
+		`O God, of your goodness, give me yourself, for you are enough for me. I can ask for nothing less that is completely to your honor, and if I do ask anything less, I shall always be in want. Only in you I have all. Amen.
+		
+Our Father, who art in heaven,
+	Hallowed be thy Name,
+	thy kingdom come,
+	thy will be done, 
+	on earth as it is in heaven.
+Give us this day our daily bread.
+And forgive us our trespasses,
+	as we forgive those
+	who trespass against us.
+And lead us not into temptation,
+	but deliver us from evil.
+For thine is the kingdom, 
+	and the power, and the glory,
+	for ever and ever. Amen.` // Saturday - Julian of Norwich
     ],
     9: [ 
         `Give me, O Lord, a steadfast heart, which no unworthy thought can drag down; an unconquered heart, which no tribulation can wear out; an upright heart, which no unworthy purpose can tempt aside. Bestow upon me understanding to know you, diligence to seek you, wisdom to find you, and faithfulness that finally may embrace you. Amen.`, // Sunday - Thomas Aquinas
