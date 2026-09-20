0b8d1f40cdca765dec8fdb2f91d696997a04a5d3
kakelso <kakelso@gmail.com>
2026-09-19 23:58:29 -0400

    Add files via upload
    Updated holy day prayers thru Nov 1. Other prayer updates.

--- prayers.js
+++ prayers.js
@@ -25,7 +25,7 @@ For thine is the kingdom,
     "peace_1": `O God, the author of peace and lover of concord, to know you is eternal life and to serve you is perfect freedom: Defend us, your humble servants, in all assaults of our enemies; that we, surely trusting in your defense, may not fear the power of any adversaries, through the might of Jesus Christ our Lord. Amen.`, // A Collect for Peace (Morning Prayer)
     "grace_1": `O Lord, our heavenly Father, almighty and everlasting God, you have brought us safely to the beginning of this day: Defend us by your mighty power, that we may not fall into sin nor run into any danger; and that, guided by your Spirit, we may do what is righteous in your sight; through Jesus Christ our Lord. Amen.`, // A Collect for Grace (Morning Prayer)
     "guidance_1": `Heavenly Father, in you we live and move and have our being: We humbly pray you so to guide and govern us by your Holy Spirit, that in all the cares and occupations of our life we may not forget you, but may remember that we are ever walking in your sight; through Jesus Christ our Lord. Amen.`, // A Collect for Guidance (Morning Prayer)
-    "endurance_1": `Almighty God, whose most dear Son went not up to joy but first he suffered pain, and entered not into glory before he was crucified: Mercifully grant that we, walking in the way of the Cross, may find it none other than the way of life and peace; through Jesus Christ your Son our Lord. Amen.`, // A Collect for Endurance (Morning Prayer)
+    "endurance_1": `Almighty God, whose most dear Son went not up to joy but first he suffered pain, and entered not into glory before he was crucified: Mercifully grant that we, walking in the way of the Cross, may find it none other than the way of life and peace; through your Son Jesus Christ our Lord. Amen.`, // A Collect for Endurance (Morning Prayer) (Modified from BCP 1928 - Public Domain)
     "morning_83": `Almighty God, you alone gave us the breath of life, and you alone can keep alive in us the holy desires you impart. We beseech you, for your compassion’s sake, to sanctify all our thoughts and endeavors, that we may neither begin an action without a pure intention nor continue it without your blessing. And grant that, having the eyes of our mind enlightened to behold things invisible and unseen, we may in heart be inspired by your wisdom, in work be upheld by your strength, and in the end be accepted as your faithful servants; through Jesus Christ our Savior. Amen.`, // 83. In the Morning
 
     // --- THIRD HOUR (Hour 3) ---
@@ -84,16 +84,70 @@ To be a light to lighten the Gentiles,
 	and to be the glory of your people Israel.`, // Nunc Dimittis: The Song of Simeon (Compline) 
 	
 	// --- ADVENT ---
-	"advent_1": `Almighty God, give us grace to cast away the works of darkness, and put on the armor of light, now in the time of this mortal life in which your Son Jesus Christ came to visit us in great humility; that in the last day, when he shall come again in his glorious majesty to judge both the living and the dead, we may rise to the life immortal; through him who lives and reigns with you and the Holy Spirit, one God, now and for ever. Amen.`, // The First Sunday in Advent
-	"advent_2": `Blessed Lord, who caused all Holy Scriptures to be written for our learning: Grant us so to hear them, read, mark, learn, and inwardly digest them, that by patience and the comfort of your holy Word we may embrace and ever hold fast the blessed hope of everlasting life, which you have given us in our Savior Jesus Christ; who lives and reigns with you and the Holy Spirit, one God, for ever and ever. Amen.`, // The Second Sunday in Advent
-	"advent_3": `O Lord Jesus Christ, you sent your messengers the prophets to preach repentance and prepare the way for our salvation: Grant that the ministers and stewards of your mysteries may likewise make ready your way, by turning the hearts of the disobedient toward the wisdom of the just, that at your second coming to judge the world, we may be found a people acceptable in your sight; for with the Father and the Holy Spirit you live and reign, one God, now and for ever. Amen.`, // The Third Sunday in Advent
-	"advent_4": `Stir up your power, O Lord, and with great might come among us; and as we are sorely hindered by our sins from running the race that is set before us, let your bountiful grace and mercy speedily help and deliver us; through Jesus Christ our Lord, to whom, with you and the Holy Spirit, be honor and glory, now and for ever. Amen.`, // The Fourth Sunday in Advent
+	//"andrew"
+	"advent_1": `Almighty God, give us grace that we may cast away the works of darkness, and put on the armor of light, now in the time of this mortal life, in which your Son Jesus Christ came to visit us in great humility; that in the last day, when he shall come again in his glorious majesty to judge both the living and the dead, we may rise to the life immortal; through him who lives and reigns with you and the Holy Spirit, one God, now and for ever. Amen.`, // The First Sunday in Advent (modified from BCP 1928 - Public Domain)
+	"advent_2": `Blessed Lord, who caused all Holy Scriptures to be written for our learning: Grant us that we may so hear them, read, mark, learn, and inwardly digest them, that by patience and the comfort of your holy Word we may embrace, and ever hold fast, the blessed hope of everlasting life, which you have given us in our Savior Jesus Christ. Amen.`, // The Second Sunday in Advent (modified from BCP 1928 - Public Domain)
+	"advent_3": `O Lord Jesus Christ, who at your first coming sent your messenger to prepare the way before you: Grant that the ministers and stewards of your mysteries may likewise so prepare and make ready your way, by turning the hearts of the disobedient toward the wisdom of the just, that at your second coming to judge the world, we may be found an acceptable people in your sight; for with the Father and the Holy Spirit you live and reign, one God, world without end. Amen.`, // The Third Sunday in Advent (modified from BCP 1928 - Public Domain)
+	//"thomas"
+	"advent_4": `O Lord, raise up your power and come among us, and with great might sustain us; and because, through our sins and wickedness, we are sorely hindered in running the race that is set before us, let your bountiful grace and mercy speedily help and deliver us; through Jesus Christ our Lord, to whom, with you and the Holy Spirit, be honor and glory, now and forever. Amen.`, // The Fourth Sunday in Advent (modified from BCP 1928 - Public Domain)
 	
-	// --- CHRISTMAS ---
-	"christmas_eve": `O God, you have caused this holy night to shine with the brightness of the true Light: Grant that we, who have known the mystery of that Light on earth, may also enjoy him perfectly in heaven; where with you and the Holy Spirit he lives and reigns, one God, in glory everlasting. Amen.`, // Christmas Eve
-	"christmas_day": `Almighty God, you have given your only-begotten Son to take our nature upon him, and to be born [this day] of a pure virgin: Grant that we, who have been born again and made your children by adoption and grace, may daily be renewed by your Holy Spirit; through Jesus Christ our Lord, to whom with you and the same Spirit be honor and glory, now and for ever. Amen.`, // Christmas Day
+	// --- CHRISTMASTIDE ---
+	"christmas_eve": `O God, you have caused this holy night to shine with the brightness of the true Light: Grant that we, who have known the mystery of that Light on earth, may also enjoy him perfectly in heaven; where with you and the Holy Spirit he lives and reigns, one God, in glory everlasting. Amen.`, // Christmas Eve, December 24
+	"christmas_day": `Almighty God, you have given your only-begotten Son to take our nature upon him, and to be born [this day] of a pure virgin: Grant that we, who have been born again and made your children by adoption and grace, may daily be renewed by your Holy Spirit; through Jesus Christ our Lord, to whom with you and the same Spirit be honor and glory, now and for ever. Amen.`, // Christmas Day, December 25
+	//"stephen"
+	//"john_ev"
+	//"innocents"
+	"holy_name": `Almighty God, your blessed Son fulfilled the covenant of circumcision for our sake, and was given the Name that is above every name: Give us grace faithfully to bear his Name, and to worship him with pure hearts according to the New Covenant; who lives and reigns with you, in the unity of the Holy Spirit, one God, now and for ever. Amen.`, // Circumcision and Holy Name, January 1
 	
+	// --- EPIPHANYTIDE ---
+	//"epiphany"
+	//"conf_peter"
+	//"conv_paul"
+	//"presentation"
 	
+	// --- WINTER AND SPRING RED-LETTER DAYS --- 
+	//"matthias"
+	//"joseph"
+	//"annunciation"
+	//"mark"
+	//"philip_james"
+	//"visitation"
+	//"barnabas"
+	
+	// --- LENT AND EASTERTIDE ---
+	//"ash_wed"
+	//"maundy_thurs"
+	//"good_fri"
+	//"holy_sat1"
+	//"holy_sat2"
+	//"easter_eve"
+	//"easter_1"
+	//"easter_2"
+	//"easter_mon"
+	//"easter_tue"
+	//"easter_wed"
+	//"easter_thu"
+	//"easter_fri"
+	//"easter_sat"
+	//"ascension"
+	//"pentecost"
+	
+	// --- SUMMER AND FALL FEASTS AND RED-LETTER DAYS --- 
+	//"trinity_sun"
+	//"n_john_bap"
+	//"peter_paul"
+	//"mary_magd"
+	//"james_elder"
+	//"transfiguration"
+	//"mary_virgin"
+	//"bartholomew"
+	//"holy_cross"
+	"matthew": `Lord Jesus, you called Matthew from collecting taxes to become your apostle and evangelist: Grant us the grace to forsake all covetous desires and inordinate love of riches, that we may follow you as he did and proclaim to the world around us the good news of your salvation; for with the Father and the Holy Spirit you live and reign, one God, now and for ever. Amen.`, // Saint Matthew, September 21
+	"michael": `Everlasting God, you have ordained and constituted in a wonderful order the ministries of angels and mortals: Mercifully grant that, as your holy angels always serve and worship you in heaven, so by your appointment they may help and defend us here on earth; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever. Amen.`, // Holy Michael and All Angels, September 29
+	"luke": `Almighty God, you called your servant Luke to be an evangelist and physician of the soul: Grant that, by the wholesome medicine of the doctrine he taught, all the diseases of our souls may be healed; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, now and for ever. Amen.`, // Saint Luke, October 18
+	"james_jer": `Grant, O God, that, following the example of your apostle James the Just, kinsman of our Lord, your Church may give itself continually to prayer and to the reconciliation of all who are at variance and enmity; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, now and for ever. Amen.`, // Saint James of Jerusalem, October 23
+	"simon_jude": `Grant, O God, that as your apostles Simon and Jude were faithful and zealous in their mission, so we may with ardent devotion make known the love and mercy of our Lord and Savior Jesus Christ; who lives and reigns with you and the Holy Spirit, one God, for ever and ever. Amen.`, // Saints Simon and Jude, October 28
+	"all_saints": `Almighty God, you have knit together your elect in one communion and fellowship in the mystical Body of your Son: Give us grace so to follow your blessed saints in all virtuous and godly living, that we may come to those ineffable joys that you have prepared for those who truly love you; through Jesus Christ our Lord, who with you and the Holy Spirit lives and reigns, one God, in glory everlasting. Amen.`, // All Saints Day, November 1
 	
 	// --- EMBER DAYS --- 
 	"ember_1": `Almighty God, the giver of all good gifts, in your divine providence you have appointed various orders in your Church: Give your grace, we humbly pray, to all who are [now] called to any office and ministry for your people; and so fill them with the truth of your doctrine and clothe them with holiness of life, that they may faithfully serve before you, to the glory of your great Name and for the benefit of your holy Church; through Jesus Christ our Lord, who lives and reigns with you, in the unity of the Holy Spirit, one God, now and for ever. Amen.`, // Ember Days
@@ -189,7 +243,7 @@ const prayerSchedule = {
 // 3. SPECIAL DATES OVERRIDE (For Future Use)
 // Format dates as "MM-DD" to override the daily schedule above.
 const specialDates = {
-    "11-29": {
+	"11-29": {
 		0: ["advent_1", "strength_return", "lords_prayer"]
 	},
 	"12-06": {
@@ -210,8 +264,12 @@ const specialDates = {
 	"12-20": {
 		0: ["advent_4", "strength_return", "lords_prayer"]
 	},
-	"12-24": {
-		12: ["christmas_eve", "lords_prayer"]
+	"12-24": {													// Christmas Eve, Thursday in 2026
+		12: ["christmas_eve", "lords_prayer"], 
+		"night_3": ["christmas_eve", "lords_prayer", "nunc_dim"], // Watch 1, hr 3
+		"night_4": ["christmas_eve", "lords_prayer", "nunc_dim"], // Watch 2, hr 1
+		"night_5": ["christmas_eve", "lords_prayer", "nunc_dim"], // Watch 2, hr 2
+		"night_6": ["christmas_eve", "lords_prayer", "nunc_dim"]  // Watch 2, hr 3 - ends at solar midnight
 	},
 	"12-25": {
         0: ["christmas_day", "lords_prayer"]
@@ -221,6 +279,44 @@ const specialDates = {
 	},
 	"01-01": {
 		//0: ["holy_name"]
+	},
+	
+	
+	"09-21": {													// Matthew, Monday in 2026
+		0: ["renew_life", "lords_prayer", "matthew"], 
+        3: ["mon_3", "matthew"], 
+        6: ["midday_3", "lords_prayer", "matthew"], 
+        9: ["mon_9", "matthew"], 
+	},
+	"09-29": {													// Michael and all Angels, Tuesday in 2026
+		0: ["peace_1", "lords_prayer", "michael"], 
+        3: ["tue_3", "michael"], 
+        6: ["know_love", "lords_prayer", "michael"], 
+        9: ["annunciation_3_25", "michael"]
+	},
+	"10-18": {													// Luke, Sunday in 2026
+		0: ["strength_return", "lords_prayer", "luke"], 
+        3: ["sun_102", "luke"], 
+        6: ["midday_2", "lords_prayer", "luke"], 
+        9: ["virtuous_heart", "luke"]
+	},
+	"10-23": {													// James of Jerusalem, Friday in 2026
+		0: ["endurance_1", "lords_prayer", "james_jer"], 
+        3: ["unity_3", "james_jer"], 
+        6: ["midday_1", "lords_prayer", "james_jer"], 
+        9: ["kingdom_115", "james_jer"]
+	},
+	"10-28": {													// Simon and Jude, Wednesday in 2026
+		0: ["grace_1", "lords_prayer", "simon_jude"], 
+        3: ["holy_thought", "simon_jude"], 
+        6: ["mission_118", "lords_prayer", "simon_jude"], 
+        9: ["evening_85", "simon_jude"]
+	},
+	"11-01": {													// All Saints, Sunday in 2026
+		0: ["strength_return", "lords_prayer", "all_saints"], 
+        3: ["sun_102", "all_saints"], 
+        6: ["midday_2", "lords_prayer", "all_saints"], 
+        9: ["virtuous_heart", "all_saints"]
 	}
 	
 };
